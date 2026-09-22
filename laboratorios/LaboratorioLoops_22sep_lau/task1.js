94721540e0a31b937b79431e4749b9997c5632e9
Laura <laurarobellada.contacto@gmail.com>
2026-09-22 16:31:07 -0600

    Completada Tarea 1: Bucles del 1 al 105

---
+++ laboratorios/LaboratorioLoops_22sep_lau/task1.js
@@ -0,0 +1,9 @@
+// Refer to Task 1 in your Instructions to complete this task
+//LAB_TAREA 1_LAURAROBELLADA
+
+for (let i = 1; i <= 105; i++) {
+    console.log(i);
+  }
+
+  //CAMBIAMOS LOS VALORES. La variable empieza a contar desde 1 y se detiene en 105, imprimiendo cada número en la consola.El bucle se imprime mientras la condición de que i sea menor o igual a 105 se cumpla.Console.log muestra el num actual que lleva la variable i en cada vuelta del bucle.
+
