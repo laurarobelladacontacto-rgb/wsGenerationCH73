9aa0d6dac1e6e335cc62a87050cd73c431987e17
Laura <laurarobellada.contacto@gmail.com>
2026-09-22 20:26:36 -0600

    Agregar archivo calculadoraJS.js

---
+++ laboratorios/LaboratorioCalculadoraJS_22sep/calculadoraJS.js
@@ -0,0 +1,19 @@
+// Parte 5: Reto extra - Saludo inicial
+let nombre = prompt("¿Cómo te llamas?");
+console.log("Hola " + nombre + ", estos son tus resultados:");
+
+// Paso 1: Solicitar los números
+let numero1 = Number(prompt("Escribe el primer número"));
+let numero2 = Number(prompt("Escribe el segundo número"));
+
+// Paso 2: Operaciones
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+// Paso 3: Mostrar los resultados
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
