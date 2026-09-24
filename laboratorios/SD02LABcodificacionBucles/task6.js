9ef6dd4b314ed79a1be13a47162d67ee7466f5c3
Laura <laurarobellada.contacto@gmail.com>
2026-09-23 21:32:49 -0600

    Completada Tarea 6: Almacenamiento en arreglo

--- laboratorios/SD02LABcodificacionBucles/task6.js
+++ laboratorios/SD02LABcodificacionBucles/task6.js
@@ -1,5 +1,33 @@
 // Refer to Task 6 in your Instructions to complete this task
+// tarea 6 23/09 laura robellada
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
-  };
+//el objetivo es cambiar la forma en que mostramos los datos: en lugar de ir imprimiendo línea por línea en la consola con console.log() a cada vuelta del bucle, vamos a ir guardando cada resultado dentro de un arreglo (array) y al final imprimimos la lista completa.
+
+// Tarea 6: Asignar los resultados de la secuencia a un array
+
+
+
+const resultados = [];
+
+  for (let i = 1; i <= 105; i++) {
+      if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+          resultados.push("FizzBuzzWoof");
+      } else if (i % 3 === 0 && i % 5 === 0) {
+          resultados.push("FizzBuzz");
+      } else if (i % 3 === 0 && i % 7 === 0) {
+          resultados.push("FizzWoof");
+      } else if (i % 5 === 0 && i % 7 === 0) {
+          resultados.push("BuzzWoof");
+      } else if (i % 3 === 0) {
+          resultados.push("Fizz");
+      } else if (i % 5 === 0) {
+          resultados.push("Buzz");
+      } else if (i % 7 === 0) {
+          resultados.push("Woof");
+      } else {
+          resultados.push(i);
+      }
+  }
+
+// Imprimimos el arreglo al finalizar el bucle
+console.log(resultados);
